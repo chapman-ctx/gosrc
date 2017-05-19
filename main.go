bedfe01db5c4b584f5def8168a8f6b08fa17c041
chapman-ctx <chapman.ctx@gmail.com>
2017-05-19 10:35:24 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+// Summary text
+//   This is some summary text
+//   *** Does it support Markldown ***?
+//   <b>Html Tags</b>?
+
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world")
+}
